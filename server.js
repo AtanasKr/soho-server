9eed35bda8d82f6a4da3a0a4ff03fc11c9af5bf2
Atanas Krstev <atanas.krstev@ipsos.com>
2024-02-12 13:34:09 +0200

    adding request logger

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const customLogger =  require('./middleware/logEvents');
 
 const PORT = process.env.PORT || 3500;
-
+app.use(customLogger);
 app.use('/', require("./routes/api/root"))
+
 
 app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
